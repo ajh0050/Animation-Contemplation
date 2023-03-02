8bc5f1d1d3a57eb2263cdea2f17cce69a46c5859
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-02 18:52:29 -0500

    Fetch and return the mapping of the quotes

--- src/Components/Quotes/Quotes.js
+++ src/Components/Quotes/Quotes.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
-
+import "./Quotes.css";
 
 class Quotes extends Component {
   constructor() {
     super();
+    this.state = {
+      quotes: [],
+    };
   }
-
+  componentDidMount() {
+    fetch(`https://animechan.vercel.app/api/quotes/character?name=${this.props.name}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ quotes: data });
+      })
+  }
   render() {
+    let quotes = this.state.quotes?.map((quote) => {
+      return (
+        <figure className="quote-container">
+          <blockquote>
+            <p className="quote">{quote.quote}</p>
+          </blockquote>
+          <figcaption>
+            &mdash; {quote.character}, <cite>{quote.anime}</cite>
+          </figcaption>
+          <button>❤️</button>
+        </figure>)
+    })
     return (
-      <div>
-        <h1>Quotes about {this.props.name}</h1>
+      <div className="quotes-container">
+        {quotes}
       </div>
     );
   }
