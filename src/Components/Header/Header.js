3d207e2a5b09fc78cb9c562bd50ddacc9937f031
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-02 20:18:18 -0500

    Add favoriting ability and deleting of favorites ability

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header className="header">
+            <Link to="/">🏠</Link>
             <h1>Animation Contemplation</h1>
+            <Link to="/favorites">Favorites</Link>
         </header>
     );
 }
