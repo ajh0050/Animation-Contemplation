a8f24a28f424c68c766f8e218d515b800ea23568
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-04 14:49:36 -0500

    Add cypress testing for search functionality on the home view

--- cypress/e2e/Home_spec.cy.js
+++ cypress/e2e/Home_spec.cy.js
@@ -12,4 +12,29 @@ describe('Home view renders all elements it should', () => {
     cy.get('.home-link').should('have.attr', 'href', '/')
     cy.get('.favorites-link').should('have.attr', 'href', '/favorites')
   })
+
+  it('The favorites button redirects to the favorites page', () => {
+    cy.get('.favorites-link').click()
+    cy.url().should('include', '/favorites')
+  })
+
+  it('The home button redirects to the home page', () => {
+    cy.get('.home-link').click()
+    cy.url().should('include', '/')
+  })
+
+  it('The search bar is present', () => {
+    cy.get('.search-title').contains('Search any anime character to get quotes from them!')
+    cy.get('.search-bar').should('have.attr', 'placeholder', 'Search for a character')
+  })
+
+  it('The search bar is functional', () => {
+    cy.get('.search-bar').type('aa')
+    cy.get('.characters-button-container').children().should('have.length', 5)
+    cy.get('.search-bar').clear()
+    cy.get('.search-bar').type('aan')
+    cy.get('.characters-button-container').children().should('have.length', 1)
+    cy.get('.character-button').contains('Aang').click()
+    cy.url().should('include', '/character/Aang')
+  })
 })
