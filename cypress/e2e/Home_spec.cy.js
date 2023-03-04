ccbfccbca4f5c11f28f6e5819f3ab741d605f47c
Adam Hughes <adamjordanhughes@gmail.com>
2023-03-04 14:23:15 -0500

    E2E the header render elements and stub the characters API

--- cypress/e2e/Home_spec.cy.js
+++ cypress/e2e/Home_spec.cy.js
@@ -1,11 +1,15 @@
 describe('Home view renders all elements it should', () => {
   beforeEach(() => {
-    cy.intercept('https://animation-contemplation-api.fly.dev/characters'), { fixture: 'characters.json'}
+    cy.intercept("GET",'https://animation-contemplation-api.fly.dev/characters',{ fixture: 'characters.json'})
     cy.visit('http://localhost:3000')
   })
   
   it('The Header elements are present', () => {
-    cy.get('h1').contains('Animation Contemplation')
-    cy.get('h2').contains('A place to contemplate your favorite anime quotes')
+    cy.get('.app-title').contains('Animation Contemplation')
+    cy.get('.app-subtitle').contains('A place to contemplate your favorite anime quotes')
+    cy.get('.home-link').contains('Home')
+    cy.get('.favorites-link').contains('Favorites')
+    cy.get('.home-link').should('have.attr', 'href', '/')
+    cy.get('.favorites-link').should('have.attr', 'href', '/favorites')
   })
 })
